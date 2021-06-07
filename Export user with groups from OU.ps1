6dc8c383dc6119a9fb0dfa71ea7003f0beaf3aff
SpoochDK <alexander.holtz@gmail.com>
2021-06-07 14:33:53 +0200

    Add files via upload

---
+++ Export user with groups from OU.ps1
@@ -0,0 +1,19 @@
+﻿Get-ADUser -Filter * -SearchBase "OU=Ritzau,OU=Users,OU=RIT - STK,DC=ritzau,DC=org" -Properties memberOf |
+     Foreach-Object{
+         # $_ represents a user object
+         $var = [PSCustomObject]@{
+                 SID = $_.SamAccountName
+                 Name = $_.Name
+                 Groups = ""
+                                  }
+         # create one row for each user, all groups in "Group" column, each separated by ','
+         if ($_.memberOf){
+             $groups = @()
+             $_.memberOf |
+                 ForEach-Object{
+                     $groups += (Get-ADGroup $_).samaccountname
+                 }
+             $var.Groups = $groups -join ' , '
+             $var
+         }
+     } | Export-Csv -Path C:\export\GrpMembers.csv -NoTypeInformation
